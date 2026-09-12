bdfe167b6867796ba5ca99a8ffa31624b9baa794
Minh-K-Vu <minh.k.vu@student.adelaide.edu.au>
2026-09-12 10:30:35 +0930

    Initial Setup

---
+++ src/main/java/com/example/speechtotext/SpeechToTextJavawebApplication.java
@@ -0,0 +1,13 @@
+package com.example.speechtotext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechToTextJavawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechToTextJavawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/speechtotext/SpeechToTextJavawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speechtotext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechToTextJavawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
